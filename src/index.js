14e9528c2071a65f5e65a720d2be3b212c0bf3e5
mrzmmr <mrzmmr@gmail.com>
2017-12-05 08:50:26 +0000

    add lint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /**
  * Remark code blocks
@@ -6,32 +6,32 @@
  * Extracts code blocks from an mdast tree and stores them in vfiles data object.
  */
 
-const findAllAfter = require('unist-util-find-all-after')
+const findAllAfter = require('unist-util-find-all-after');
 
 module.exports = (options = {}) => {
   return (tree, file) => {
-    const { values, lang } = options
-    let codeblocks
-    let nodes = []
+    const { values, lang } = options;
+    let codeblocks;
+    let nodes = [];
 
-    nodes = findAllAfter(tree, 0, 'code')
+    nodes = findAllAfter(tree, 0, 'code');
 
     if (tree.children[0].type === 'code') {
-      nodes.unshift(tree.children[0])
+      nodes.unshift(tree.children[0]);
     }
 
-    codeblocks = nodes
+    codeblocks = nodes;
 
     if (lang) {
-      nodes = codeblocks
-      codeblocks = nodes.filter(node => node.lang === lang)
+      nodes = codeblocks;
+      codeblocks = nodes.filter(node => node.lang === lang);
     }
 
     if (values) {
-      nodes = codeblocks
-      codeblocks = nodes.map(node => node.value)
+      nodes = codeblocks;
+      codeblocks = nodes.map(node => node.value);
     }
 
-    file.data.codeblocks = codeblocks
-  }
-}
+    file.data.codeblocks = codeblocks;
+  };
+};
